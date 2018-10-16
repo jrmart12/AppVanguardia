8b4de271733c85298528a3494e1cb26ddf11df61
jose <rodolfojose@live.com>
2018-10-16 14:51:42 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+    return React.createElement(
+      "div",
+      {},
+      React.createElement("h1", {}, "Lista de proyectos de GitHub!")
+    );
+  };
+  
+  ReactDOM.render(React.createElement(App), document.getElementById("root"));
